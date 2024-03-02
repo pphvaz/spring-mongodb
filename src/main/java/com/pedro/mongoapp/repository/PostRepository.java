cf295e6e5542c666bfe7ea4d6eb53d7b89d99806
Pedro Alves <pedrohvs.alves@gmail.com>
2024-03-02 08:26:07 +0000

    Consulta simples com @Query

--- src/main/java/com/pedro/mongoapp/repository/PostRepository.java
+++ src/main/java/com/pedro/mongoapp/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.pedro.mongoapp.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedro.mongoapp.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0/pattern/, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/pedro/mongoapp/services/UserService.java
+++ src/main/java/com/pedro/mongoapp/services/UserService.java
@@ -34,7 +34,6 @@ public class UserService {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
 		
 	public User update(User obj) {
 		User newObj = findById(obj.getId());
@@ -42,8 +41,6 @@ public class UserService {
 		return repo.save(newObj);
 	}
 
-	
-	
 	private void updateData(User newObj, User obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
